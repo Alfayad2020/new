8c25b1c4610f9313e511260c5c73c4a51aefb1ed
Alfayad2020 <hadizajs2018@gmail.com>
2024-10-29 03:57:42 -0700

    my rep

---
+++ MY/test.js
@@ -0,0 +1,19 @@
+//create an empty arrays
+let todolist = [];
+//get all the items you need
+const inputItem = document.getElementById("Newitem");
+const btn = document.getElementById("btn");
+const listContainer = document.getElementById("list");
+//create a function to display a list item
+function displayList() {}
+const display = () =>{
+    //clear existing list item to avoide dublicates
+    listContainer.innerHTML = "";
+    console.log("listContainer");
+    //loop through the array and call the item
+    todolist.forEach((item) =>{
+        const listItem = document.createElement("list")
+    }
+    
+    )
+}
